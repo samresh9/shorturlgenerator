51f31606c33dcc148c6b3ba88e7d5ead119c255d
samresh9 <thapasamresh9@gmail.com>
2023-02-24 18:58:26 +0545

    jwt implement

--- service/auth.js
+++ service/auth.js
@@ -1,13 +1,17 @@
-const sessionIdToUserMap = new Map();
 
-function setUser(id,  user){
-
-    sessionIdToUserMap.set( id , user);
+const jwt = require("jsonwebtoken");
+const jwtSecret = "keytosigntoken";
+function setUser( user){
+    const payload = {
+        id: user._id,
+        email: user.email
+    };
+ return jwt.sign(payload , jwtSecret);
 };
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
-    
+function getUser(token){
+   if(!token) return null;
+  return  jwt.verify(token , jwtSecret)
 };
 
 module.exports = {
